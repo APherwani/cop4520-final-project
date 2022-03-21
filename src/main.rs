526523a34c2d53464441493575dc7060146afb4b
ctcuff <dev.ctcuff@gmail.com>
2022-03-21 14:51:04 -0400

    Files are split into chunks and encrypted

--- src/main.rs
+++ src/main.rs
@@ -3,63 +3,64 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 
+use async_std::task;
+use chacha20poly1305::aead::{Aead, NewAead};
+use chacha20poly1305::{ChaCha20Poly1305, Key, Nonce};
+use itertools::Itertools;
+use std::cmp;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-use chacha20poly1305::aead::{Aead, NewAead};
-use chacha20poly1305::{ChaCha20Poly1305, Key, Nonce};
-
-const CHUNK_SIZE: usize = 5;
+const CHUNK_SIZE: usize = 500;
 
 fn main() {
-    // // Create a path to the desired file
-    // let path = Path::new("../very_large_file.txt");
-    // let display = path.display();
+    // Create a path to the desired file
+    let path = Path::new("./medium_sized_file.txt");
+    let display = path.display();
 
-    // // Open the path in read-only mode, returns `io::Result<File>`
-    // let mut file = match File::open(&path) {
-    //     Err(why) => panic!("couldn't open {}: {}", display, why),
-    //     Ok(file) => file,
-    // };
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
 
-    // // Read the file contents into a string, returns `io::Result<usize>`
-    // let mut s = String::new();
-    // match file.read_to_string(&mut s) {
-    //     Err(why) => panic!("couldn't read {}: {}", display, why),
-    //     Ok(_) => println!("Read new file containing {} characters.", s.len()),
-    // }
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => println!("Read new file containing {} characters.", s.len()),
+    }
 
     // Must be 32 bytes
     let encryption_key = "2c26481d-4c16-4985-a5bb-d7120eb2";
     // Must be 12 bytes
     let nonce_key = "4428fa183b99";
 
-    let message = "Apparently motionless to her passengers and crew
-    the Interplanetary liner Hyperion bored serenely onward through
-    space at normal acceleration. In the railed-off sanctum in one
-    corner of the control room a bell tinkled, a smothered whirr was
-    heard, and Captain Bradley frowned as he studied the brief message
-    upon the tape of the recorder--a message flashed to his desk
-    from the operator's panel.";
-
     let cipher_key = Key::from_slice(encryption_key.as_bytes());
     let cipher = ChaCha20Poly1305::new(cipher_key);
     let nonce = Nonce::from_slice(nonce_key.as_bytes());
 
-    encrypt_file("../encrypted.bin", &message, &cipher, &nonce);
-    decrypt_file("../encrypted.bin", &cipher, &nonce);
+    let chunks = split_text(&s);
+
+    match std::fs::create_dir_all("./encrypted") {
+        Ok(_) => (),
+        Err(why) => println!("Failed to created directory: {}", why),
+    }
+
+    for (index, chunk) in chunks.iter().enumerate() {
+        let filename = format!("./encrypted/chunk-{}.bin", index);
+        encrypt_file(&filename, &chunk, &cipher, &nonce);
+    }
 }
 
-fn split_file(s: &String) {
-    for i in (0..100).step_by(CHUNK_SIZE) {
-        println!(
-            "s[{}..{}] = \n**{}**",
-            i,
-            i + CHUNK_SIZE,
-            &s[i..(i + CHUNK_SIZE)]
-        );
-    }
+fn split_text(s: &String) -> Vec<String> {
+    return s
+        .chars()
+        .chunks(CHUNK_SIZE)
+        .into_iter()
+        .map(|chunk| chunk.collect::<String>())
+        .collect::<Vec<String>>();
 }
 
 fn encrypt_file(filename: &str, plaintext: &str, cipher: &ChaCha20Poly1305, nonce: &Nonce) {
@@ -76,7 +77,7 @@ fn encrypt_file(filename: &str, plaintext: &str, cipher: &ChaCha20Poly1305, nonc
     }
 }
 
-fn decrypt_file(filename: &str, cipher: &ChaCha20Poly1305, nonce: &Nonce) {
+fn decrypt_file(filename: &str, cipher: &ChaCha20Poly1305, nonce: &Nonce) -> String {
     let path = Path::new(filename);
     let display = path.display();
     let mut file = match File::open(&path) {
@@ -92,9 +93,12 @@ fn decrypt_file(filename: &str, cipher: &ChaCha20Poly1305, nonce: &Nonce) {
     }
 
     let plaintext = match cipher.decrypt(nonce, buffer.as_ref()) {
-        Ok(value) => String::from_utf8(value).expect("Couldn't decrypt text from file"),
+        Ok(value) => {
+            let error_message = format!("Couldn't decrypt text from file: {}", filename);
+            String::from_utf8(value).expect(&error_message)
+        }
         Err(why) => panic!("Failed to decrypt text: {}", why),
     };
 
-    println!("Decrypted text: {}", plaintext);
+    return plaintext;
 }
