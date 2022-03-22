ae3384f08e6b812ee3d0d1161498af6b1cb6a566
ctcuff <dev.ctcuff@gmail.com>
2022-03-21 23:05:39 -0400

    Encryption key is saved to a JSON file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-#![allow(dead_code)]
-#![allow(unused_mut)]
-#![allow(unused_variables)]
-#![allow(unused_imports)]
-
-use async_std::task;
 use chacha20poly1305::aead::{Aead, NewAead};
 use chacha20poly1305::{ChaCha20Poly1305, Key, Nonce};
 use itertools::Itertools;
-use std::cmp;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use uuid::Uuid;
+
+/// Contains the main encryption key used for the `ChaCha20Poly1305` cipher
+/// along with a map of nonce keys used to encrypt file chunks.
+#[derive(Deserialize, Serialize, Debug)]
+struct KeyStore {
+    /// This is the master encryption key used for the cipher. It must be 32 bytes.
+    encryption_key: String,
+    /// In this hashmap, the key refers to the name of the encrypted file
+    /// and the value refers to the nonce key used to encrypt the file.
+    /// All must be 24 bytes.
+    nonce: HashMap<String, String>,
+}
 
 const CHUNK_SIZE: usize = 500;
+const ENCRYPTION_DIR: &str = "./encrypted";
 
 fn main() {
     // Create a path to the desired file
@@ -32,25 +41,47 @@ fn main() {
         Ok(_) => println!("Read new file containing {} characters.", s.len()),
     }
 
-    // Must be 32 bytes
-    let encryption_key = "2c26481d-4c16-4985-a5bb-d7120eb2";
-    // Must be 12 bytes
-    let nonce_key = "4428fa183b99";
+    let mut keystore = KeyStore {
+        encryption_key: Uuid::new_v4().to_string()[..32].to_string(),
+        nonce: HashMap::new(),
+    };
 
-    let cipher_key = Key::from_slice(encryption_key.as_bytes());
+    let cipher_key = Key::from_slice(keystore.encryption_key.as_bytes());
     let cipher = ChaCha20Poly1305::new(cipher_key);
-    let nonce = Nonce::from_slice(nonce_key.as_bytes());
-
     let chunks = split_text(&s);
 
-    match std::fs::create_dir_all("./encrypted") {
+    match std::fs::create_dir_all(ENCRYPTION_DIR) {
         Ok(_) => (),
-        Err(why) => println!("Failed to created directory: {}", why),
+        Err(why) => panic!("Failed to created directory: {why}"),
     }
 
     for (index, chunk) in chunks.iter().enumerate() {
-        let filename = format!("./encrypted/chunk-{}.bin", index);
-        encrypt_file(&filename, &chunk, &cipher, &nonce);
+        let filename = format!("{ENCRYPTION_DIR}/{index}_{}.bin", Uuid::new_v4().to_string());
+        let nonce_key = Uuid::new_v4().to_string()[24..].to_string();
+
+        encrypt_file(
+            &filename,
+            &chunk,
+            &cipher,
+            nonce_key.as_ref(),
+        );
+
+        keystore.nonce.insert(filename, nonce_key);
+    }
+
+    let key_json =
+        serde_json::to_string_pretty(&keystore).expect("Failed to serialize encryption keys.");
+    let path = format!("{ENCRYPTION_DIR}/keystore.json");
+    let keystore_path = Path::new(&path);
+    
+    let mut keystore_file = match File::create(keystore_path) {
+        Ok(file) => file,
+        Err(why) => panic!("Failed to keystore {}: {why}", keystore_path.display())
+    };
+
+    match keystore_file.write_all(&key_json.to_string().as_bytes()) {
+        Ok(_) => println!("Key file saved to: {}", keystore_path.display()),
+        Err(why) => println!("Error saving {}: {why}", keystore_path.display()),
     }
 }
 
@@ -63,41 +94,43 @@ fn split_text(s: &String) -> Vec<String> {
         .collect::<Vec<String>>();
 }
 
-fn encrypt_file(filename: &str, plaintext: &str, cipher: &ChaCha20Poly1305, nonce: &Nonce) {
+fn encrypt_file(filename: &str, plaintext: &str, cipher: &ChaCha20Poly1305, nonce_key: &str) {
+    let nonce = Nonce::from_slice(nonce_key.as_bytes());
     let ciphertext = match cipher.encrypt(nonce, plaintext.as_ref()) {
         Ok(value) => value,
-        Err(why) => panic!("Failed to encrypt text: {}", why),
+        Err(why) => panic!("Failed to encrypt text: {why}"),
     };
 
-    let mut file = File::create(filename).expect("Failed to open file");
+    let mut file = File::create(filename).expect(&format!("Failed to open file: {filename}"));
 
     match file.write(&ciphertext) {
-        Ok(n) => println!("{} saved => {} bytes", filename, n),
-        Err(why) => println!("Error saving encrypted file: {}", why),
+        Ok(n) => println!("{filename} saved => {n} bytes"),
+        Err(why) => println!("Error saving encrypted file: {why}"),
     }
 }
 
-fn decrypt_file(filename: &str, cipher: &ChaCha20Poly1305, nonce: &Nonce) -> String {
+#[allow(unused)]
+fn decrypt_file(filename: &str, cipher: &ChaCha20Poly1305, nonce_key: &str) -> String {
     let path = Path::new(filename);
     let display = path.display();
     let mut file = match File::open(&path) {
-        Err(why) => panic!("Couldn't open {}: {}", display, why),
+        Err(why) => panic!("Couldn't open file {display}: {why}"),
         Ok(file) => file,
     };
 
     let mut buffer: Vec<u8> = Vec::new();
 
     match file.read_to_end(&mut buffer) {
-        Err(why) => panic!("Couldn't read {}: {}", display, why),
+        Err(why) => panic!("Couldn't read {display}: {why}"),
         Ok(_) => (),
     }
 
+    let nonce = Nonce::from_slice(nonce_key.as_bytes());
     let plaintext = match cipher.decrypt(nonce, buffer.as_ref()) {
         Ok(value) => {
-            let error_message = format!("Couldn't decrypt text from file: {}", filename);
-            String::from_utf8(value).expect(&error_message)
+            String::from_utf8(value).expect(&format!("Couldn't decrypt text from file: {filename}"))
         }
-        Err(why) => panic!("Failed to decrypt text: {}", why),
+        Err(why) => panic!("Failed to decrypt text: {why}"),
     };
 
     return plaintext;
