a5f7762dfd5581336a4bdf230b2f29c52dce4b2d
Arjun Pherwani <arjunp@knights.ucf.edu>
2022-03-23 12:47:08 -0400

    delete from s3 bucket

--- src/aws.rs
+++ src/aws.rs
@@ -27,13 +27,13 @@ pub async fn write_to_bucket(filename: &str, content: Vec<u8>) {
     println!("Code is {}", _code);
 }
 
-async fn read_from_bucket(filename: &str) -> Vec<u8> {
+pub async fn read_from_bucket(filename: &str) -> Vec<u8> {
     let access_key: String = env::var("ACCESS_KEY").expect("Missing variable ACCESS_KEY");
     let secret_key: String = env::var("SECRET_KEY").expect("Missing variable SECRET_KEY");
 
     let credentials = match Credentials::new(Some(&access_key), Some(&secret_key), None, None, None)
     {
-        Err(why) => panic!("Invalid creds. Error thrown {}", why),
+        Err(why) => panic!("Invalid credentials. Error thrown {why}"),
         Ok(credentials) => credentials,
     };
 
@@ -42,7 +42,7 @@ async fn read_from_bucket(filename: &str) -> Vec<u8> {
         .parse()
         .expect("Something went wrong parsing region.");
     let bucket = Bucket::new(bucket_name, region, credentials)
-        .expect("Something went wrong creating the bucket.");
+        .expect("Something went wrong initializing the bucket.");
 
     // Async variant with `tokio` or `async-std` features
     let (data, _code) = bucket
@@ -51,4 +51,30 @@ async fn read_from_bucket(filename: &str) -> Vec<u8> {
         .expect("Something went wrong fetching the data. Code {_code}");
 
     return data;
+}
+
+pub async fn delete_object(path: &str) -> u16 {
+    let access_key = env::var("ACCESS_KEY").expect("Missing variable ACCESS_KEY");
+    let secret_key = env::var("SECRET_KEY").expect("Missing variable SECRET_KEY");
+    
+    let credentials = match Credentials::new(Some(&access_key), Some(&secret_key), None, None, None)
+    {
+        Err(why) => panic!("Invalid credentials. Error thrown {why}"),
+        Ok(credentials) => credentials,
+    };
+
+    let bucket_name = "cop4520-final-project-bucket";
+    let region = "us-east-1"
+        .parse()
+        .expect("Something went wrong parsing the region");
+    let bucket = Bucket::new(bucket_name, region, credentials)
+        .expect("Something went wrong initializing bucket.");
+
+    // Delete encrypted folder in the bucket.
+    let (_, code) = bucket
+        .delete_object(path)
+        .await
+        .expect("Something went wrong deleting the encrypted folder. Code {code}");
+
+    return code;
 }
